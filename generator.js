b2ff5b1038f78aa1f82a807637457f0f5edf0365
Ignacio Ildefonso de Miguel Ruano <ignacio.demiguel@estudiante.uam.es>
2024-07-28 20:15:03 +0200

    Added a generator.js file. This file will contain the algorithm that the user will specify to generate their password

---
+++ generator.js
@@ -0,0 +1,12 @@
+// generator.js
+function generate(webname) {
+    // Initial simple algorithm example
+    if (!webname) {
+        return "Not defined";  // Return "Not defined" if no webname is provided
+    }
+
+    // Simple transformation for demonstration: reverse the webname
+    // const reversedWebname = webname.split('').reverse().join('');
+
+    return "Not defined"
+}
